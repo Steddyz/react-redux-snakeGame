cd6c63aac550cd5598720800ddd84747a90a7f69
Steddy <qwesteddyoff@gmail.com>
2024-08-16 13:15:56 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
-import { switchButton } from "./store/gameSlice";
+import { moveSnake, switchButton } from "./store/gameSlice";
 import Board from "./components/Board/Board";
+import { useRef } from "react";
 
 function App() {
-  const { toggleButton } = useSelector((store) => store.game);
-
+  const { status } = useSelector((store) => store.game);
   const dispatch = useDispatch();
 
+  let timer = useRef(null);
+
+  const update = () => {
+    dispatch(moveSnake());
+  };
+
+  const startTimer = () => {
+    timer.current = setInterval(() => update(), 200);
+  };
+
+  const stopTimer = () => clearInterval(timer.current);
+
   const handleClick = () => {
+    if (status !== "Pause") {
+      startTimer();
+    } else {
+      stopTimer();
+    }
+
     dispatch(switchButton("Resume"));
   };
 
@@ -15,7 +33,7 @@ function App() {
     <div className="App">
       <Board />
       <button className="start_button" onClick={handleClick}>
-        {toggleButton}
+        {status}
       </button>
     </div>
   );
--- src/store/gameSlice.js
+++ src/store/gameSlice.js
@@ -3,24 +3,37 @@ import { createSlice } from "@reduxjs/toolkit";
 const gameSlice = createSlice({
   name: "game",
   initialState: {
-    toggleButton: "Start",
+    status: "Start",
+    buttonsValue: {
+      Start: "Pause",
+      Pause: "Resume",
+      Resume: "Pause",
+    },
     snake: [
       { x: 0, y: 0 },
       { x: 1, y: 0 },
     ],
+    snakeSize: 2,
     apple: {
       x: 1,
       y: 1,
     },
   },
   reducers: {
-    switchButton(state, action) {
-      state.toggleButton = action.payload;
-      //   state.toggleButton = state.toggleButton === "Start" ? "Pause" : "Start";
+    switchButton(state) {
+      state.status = state.buttonsValue[state.status];
+    },
+    moveSnake(state, action) {
+      let { x, y } = state.snake.slice(-1)[0];
+
+      x = x >= 9 ? 0 : x + 1;
+
+      state.snake.push({ x, y });
+      state.snake = state.snake.slice(-state.snakeSize);
     },
   },
 });
 
-export const { switchButton } = gameSlice.actions;
+export const { switchButton, moveSnake } = gameSlice.actions;
 
 export const gameReducer = gameSlice.reducer;
